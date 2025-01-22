ee229eaf4fe5ff82db3b1eb0084feddc5749fee2
arpitXCRM <arpit@promoxcrm.ai>
2025-01-23 01:07:57 +0530

    store

--- src/modules/organization/service.ts
+++ src/modules/organization/service.ts
@@ -2,7 +2,7 @@ import { MedusaService } from "@medusajs/framework/utils"
 import  Organization  from "./models/organization";
 
 class OrganizationService extends MedusaService({
-  Organization,
+  Organization, 
 }) {
 
 }
