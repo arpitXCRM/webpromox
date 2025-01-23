0f0583fb53801d0ddf194327cbc96042dc40e2e9
arpitXCRM <arpit@promoxcrm.ai>
2025-01-23 12:36:36 +0530

    bug fixing

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,7 +4,6 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
